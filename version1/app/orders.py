6b3baaaa1c2af9e50eed16c610ec1f9daf5e7aec
Vincent <vincentmuriuki42@gmail.com>
2018-09-19 00:26:02 +0300

    Feature (Description) Add the GET API endpoint
    The endpoint retrieves all customer orders
    
    [finishes #160544083]

--- version1/app/orders.py
+++ version1/app/orders.py
@@ -11,3 +11,7 @@ class PostOrder(Resource):
         food_orders.append(order)
 
         return ({'Message' : 'Congratulations. Your new order has been posted. Kindly wait!'}), 201
+
+    class GetOrders(Resource):
+        def get(self):
+            return {"orders":[order.virtualize() for order in food_orders]}
