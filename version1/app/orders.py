a51cfff55b02c7e05642c7d02949dd0a8019b3b6
Vincent <vincentmuriuki42@gmail.com>
2018-09-19 00:36:04 +0300

    Feature (Description) Add the GET API endpoint(single_order)
    This endpoint fetches a specific order from the datastructure by the order_id
    
    [finishes #160543903]

--- version1/app/orders.py
+++ version1/app/orders.py
@@ -12,6 +12,16 @@ class PostOrder(Resource):
 
         return ({'Message' : 'Congratulations. Your new order has been posted. Kindly wait!'}), 201
 
-    class GetOrders(Resource):
-        def get(self):
-            return {"orders":[order.virtualize() for order in food_orders]}
+class GetOrders(Resource):
+    def get(self):
+        return {"orders":[order.virtualize() for order in food_orders]}
+
+
+class SingleOrder(Resource):
+    def get(self, id):
+        spec_order = CustomerOrder().retrieve_order_by_id(id)
+        
+        if spec_order:
+            return {"order": spec_order.virtualize()}, 200
+
+        return {"message":"Order not found"}, 404
